3b89625c2e4a694df256ef949926dda3881f84fe
peudias <pedro.dias@aluno.cefetmg.br>
2024-10-24 21:01:12 -0300

    refactor[ma]: colocando memory access  no pipeline

--- src/includes/Pipeline.hpp
+++ src/includes/Pipeline.hpp
@@ -12,6 +12,7 @@
 #include "Execute.hpp"
 
 void Pipeline(Registers& regs, RAM& ram, UnidadeControle& uc, int& PC, const string& filename, const string& regsFilename);
+void MemoryAccess(const DecodedInstruction& decoded, int& resultado, RAM& ram);
 void WriteBack(const DecodedInstruction& decoded, int resultado, Registers& regs);
 
 #endif
--- src/pipeline/Pipeline.cpp
+++ src/pipeline/Pipeline.cpp
@@ -29,6 +29,18 @@ void Pipeline(Registers& regs, RAM& ram, UnidadeControle& uc, int& PC, const str
     }
 }
 
+void MemoryAccess(const DecodedInstruction& decoded, int& resultado, RAM& ram) {
+    if (decoded.opcode == LOAD) {
+        // Lê da RAM no endereço especificado pelo valor1 e coloca no registrador de destino
+        resultado = ram.read(decoded.value1); 
+        std::cout << "[MEM]: LOAD da RAM[" << decoded.value1 << "] -> Resultado: " << resultado << std::endl;
+    } else if (decoded.opcode == STORE) {
+        // Escreve na RAM no endereço value1 o valor de value2
+        ram.write(decoded.value1, decoded.value2); 
+        std::cout << "[MEM]: STORE na RAM[" << decoded.value1 << "] = " << decoded.value2 << std::endl;
+    }
+}
+
 void WriteBack(const DecodedInstruction& decoded, int resultado, Registers& regs) {
     //if (decoded.opcode == ADD || decoded.opcode == SUB || decoded.opcode == LOAD) {
     regs.set(decoded.destiny, resultado); // Escreve o resultado no registrador de destino
