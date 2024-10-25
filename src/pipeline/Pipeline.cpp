23a3d8d75c1944a82bf690ade63699de2702dcdf
peudias <pedro.dias@aluno.cefetmg.br>
2024-10-24 21:13:55 -0300

    refactor[filereader]: movendo FL p pipeline

--- src/pipeline/Pipeline.cpp
+++ src/pipeline/Pipeline.cpp
@@ -1,6 +1,9 @@
 #include "../includes/Pipeline.hpp"
-#include "../includes/FileReader.hpp"
-
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 
 using namespace std;
@@ -57,4 +60,64 @@ void WriteBack(const DecodedInstruction& decoded, int resultado, Registers& regs
     //if (decoded.opcode == ADD || decoded.opcode == SUB || decoded.opcode == LOAD) {
     regs.set(decoded.destiny, resultado); // Escreve o resultado no registrador de destino
     //}
+}
+
+void setRegistersFromFile(Registers& regs, const string& regsFilename) {
+    ifstream regsFile(regsFilename);
+    if (!regsFile.is_open()) {
+        cerr << "Erro ao abrir o arquivo de registradores: " << regsFilename << endl;
+        return;
+    }
+
+    string line;
+    while (getline(regsFile, line)) {
+        int regNum, regValue;
+        char virgula;
+        stringstream ss(line);
+
+        ss >> regNum >> virgula >> regValue;
+        regs.set(regNum, regValue);
+    }
+    regsFile.close();
+}
+
+int loadInstructionsFromFile(RAM& ram, const string& instrFilename) {
+    ifstream file(instrFilename);
+    if (!file.is_open()) {
+        cerr << "Erro ao abrir o arquivo de instruções: " << instrFilename << endl;
+        return -1;
+    }
+
+    string line;
+    int instructionAddress = 0;
+
+    while (getline(file, line)) {
+        string opcodeStr;
+        int reg1, reg2, reg3;
+        char virgula;
+
+        stringstream ss(line);
+
+        getline(ss, opcodeStr, ',');
+
+        opcodeStr.erase(remove_if(opcodeStr.begin(), opcodeStr.end(), ::isspace), opcodeStr.end());
+
+        ss >> reg1 >> virgula >> reg2 >> virgula >> reg3;
+
+        Opcode opcode;
+        if (opcodeStr == "ADD") opcode = ADD;
+        else if (opcodeStr == "SUB") opcode = SUB;
+        else if (opcodeStr == "STORE") opcode = STORE;
+        else if (opcodeStr == "LOAD") opcode = LOAD;
+        else {
+            cerr << "Instrução inválida no arquivo: " << opcodeStr << endl;
+            continue;
+        }
+
+        ram.writeInstruction(instructionAddress++, Instruction(opcode, reg1, reg2, reg3));
+    }
+
+    file.close();
+
+    return instructionAddress;
 }
